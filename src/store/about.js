28c793671a7f4778c00d35e0c08320f1353db8cf
ksunnyoo <sunkbrigita@gmail.com>
2023-01-16 17:08:13 +0900

    Create project

---
+++ src/store/about.js
@@ -0,0 +1,10 @@
+import { Component, Store } from "../core/heropy"
+
+export default new Store ({
+  photo: 'https://heropy.blog/css/images/logo.png',
+  name: 'Heropy/ parkyoungwoong',
+  email: 'thesecon@gmail.com',
+  blog: 'https://heropy.blog',
+  github: 'https://github.com/parkyounwoong',
+  repository: 'https://github.com/parkyoungwoong/vanillajs-movie-app'
+})
